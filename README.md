5d1980ad89564d8f9306e5bcf2d42a91cd4aff8b
高良真穂 <maho@maho.local>
2020-07-20 17:39:34 +0900

    update liveness & readiness probe

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# web-java
+# Spring Boot アクチュエータによる Liveness Prove と Readiness Prove の実装
+
+
+Spring Boot Actuator を利用して、Kubernetes の Livness Prove と Readiness Prove に対応する実装を検証したコードである。
+
+Kubernetesにデプロイ後、30秒後に、Liveness Prove へ HTTPレスポンス 200 を返すようになる。それまでの間は、
+
+
 
 ## コンテナイメージのDocker Hub への登録
 
--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -2,12 +2,13 @@ package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        ConfigurableApplicationContext context
+	    = SpringApplication.run(ServingWebContentApplication.class, args);
     }
-
 }
